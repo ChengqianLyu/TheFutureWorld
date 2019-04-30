de89c31309944f175b7b4ccee5f0bcb5c751ab69
MongoSP <48965695+MongoSP@users.noreply.github.com>
2019-04-30 16:44:43 -0400

    Began enemy movement AI
    No real AI, but The movement of the enemies has begun.  It's a "fake it till I make it" sort of code: An invisible collider detects if the robot has crossed, and the robot then switches which direction it's moving towards.

--- TheFutureWorld/Assets/Script/EnemyMovement.cs
+++ TheFutureWorld/Assets/Script/EnemyMovement.cs
@@ -6,17 +6,17 @@ public class EnemyMovement : MonoBehaviour {
     [Header("Set in Inspector")]
     public float speed = 1f;
     // Use this for initialization
+    private int directionCode = 1; //1 is +x, 2 is -x, 3 is +z, 4 is -z.
     private GameObject lastTriggerGo = null;
+
     void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-         Vector3 pos = transform.position;
-         pos.x += speed * Time.deltaTime;
-         transform.position = pos;
+        Move();
+         
         //Check page 573.
         //transform.Translate(Vector3.forward * Time.deltaTime);
         //transform.Rotate(0, 1f, 0, Space.Self);
@@ -31,7 +31,30 @@ public class EnemyMovement : MonoBehaviour {
             return;
         }
         lastTriggerGo = go;
-        this.transform.Rotate(0, 90, 0, Space.Self);
+        changeDirection();
+        this.transform.Rotate(0, -90, 0, Space.Self);
         print("Should have triggered");
+    }
+
+    void Move()
+    {
+        Vector3 pos = transform.position;
+        if (directionCode == 1)
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        else if (directionCode == 2)
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+         transform.position = pos;
+    }
+
+    void changeDirection()
+    {
+        if(directionCode == 1)
+        {
+            directionCode = 2;
+        }
     }
 }
