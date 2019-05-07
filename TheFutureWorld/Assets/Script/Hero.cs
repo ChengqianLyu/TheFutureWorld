48402047f20300337a7510e0a18d5d23d30d9389
ChengqianLyu <lyuc@rose-hulman.edu>
2019-05-07 15:17:15 -0400

    add one color and one script

---
+++ TheFutureWorld/Assets/Script/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        if(go.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
