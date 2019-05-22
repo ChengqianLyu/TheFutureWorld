4a3f34d7d58471f8ba57012c899227f2b64d613e
Zijian Huang <lhq8722861@gmail.com>
2019-05-21 23:24:27 -0400

    added enemy AI

---
+++ TheFutureWorld/Assets/Script/EnemyAI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+    private AggroDetection AggroDetection;
+    private Transform target;
+
+    private void Awake()
+    {
+        AggroDetection = GetComponent<AggroDetection>();
+        AggroDetection.OnAggro += AggroDetection_OnAggro;
+    }
+
+    private void AggroDetection_OnAggro(Transform target)
+    {
+        this.target = target;
+    }
+
+    private void Update()
+    {
+        if(target != null)
+        {
+            GetComponent<NavMeshAgent>().SetDestination(target.position);
+        }
+    }
+
+}
